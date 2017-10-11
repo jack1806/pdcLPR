a25d8aebcab2101c8370ca4e6281c21db27e7d3d
jack1806 <yashtodkar2@gmail.com>
2017-10-11 11:20:37 +0530

    Screenshot added

--- Main.py
+++ Main.py
@@ -73,7 +73,7 @@ def main():
 
     # end if else
 
-    cv2.waitKey(0)					# hold windows open until user presses a key
+    #cv2.waitKey(0)					# hold windows open until user presses a key
 
     return
 # end main
